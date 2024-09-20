9c631f4258bfaaf424c4a6636a468721679718c4
Piotr Gielerak <piotr@gielerak.pl>
2024-09-20 09:42:15 +0200

    init

---
+++ src/main/java/ai/starboost/helloworld/Function.java
@@ -0,0 +1,25 @@
+package ai.starboost.helloworld;
+
+import com.microsoft.azure.functions.*;
+import java.util.Optional;
+
+public class Function {
+
+    @FunctionName("HelloWorld")
+    public HttpResponseMessage run(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.GET},
+            authLevel = AuthorizationLevel.ANONYMOUS)
+        HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Return a simple "Hello, World!" response
+        return request.createResponseBuilder(HttpStatus.OK)
+            .body("Hello, World!")
+            .build();
+    }
+}
+
