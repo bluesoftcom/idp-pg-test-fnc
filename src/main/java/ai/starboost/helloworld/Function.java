6667ba2d34a1abba0728d6771b1e188d91e828a8
Piotr Gielerak <piotr@gielerak.pl>
2024-09-20 09:45:31 +0200

    Fix dependencies

--- src/main/java/ai/starboost/helloworld/Function.java
+++ src/main/java/ai/starboost/helloworld/Function.java
@@ -1,25 +1,29 @@
 package ai.starboost.helloworld;
 
 import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
 import java.util.Optional;
 
 public class Function {
 
     @FunctionName("HelloWorld")
     public HttpResponseMessage run(
-        @HttpTrigger(
-            name = "req",
-            methods = {HttpMethod.GET},
-            authLevel = AuthorizationLevel.ANONYMOUS)
-        HttpRequestMessage<Optional<String>> request,
-        final ExecutionContext context) {
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
 
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Return a simple "Hello, World!" response
         return request.createResponseBuilder(HttpStatus.OK)
-            .body("Hello, World!")
-            .build();
+                .body("Hello, World!")
+                .build();
     }
 }
 
